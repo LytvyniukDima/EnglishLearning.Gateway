72de6659291c85dfb122f4f2c1553d603c2905b2
dimaLytvyniuk <dima.lytvyniuk@gmail.com>
2018-09-17 22:55:58 +0300

    Add host project

---
+++ EnglishLearning.Gateway.Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnglishLearning.Gateway.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                .ConfigureAppConfiguration(
+                    ic => ic.AddJsonFile(Path.Combine("configuration",
+                        "configuration.json")))
+                .UseStartup<Startup>();
+
+            return builder;
+        }
+    }
+}
