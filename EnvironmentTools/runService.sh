0699641a2b34c3979b8e3138f9f1e6f24d51e9eb
dimaLytvyniuk <dima20-12@outlook.com>
2019-02-17 18:19:06 +0200

    Update to net core 2.0

--- EnvironmentTools/runService.sh
+++ EnvironmentTools/runService.sh
@@ -2,6 +2,7 @@ imageName="english_gateway"
 containerName="english_gateway_service"
 networkName="english-net"
 
+docker kill $containerName
 docker rm $containerName
 
 docker run -p 5000:5000 --name $containerName --network $networkName $imageName
